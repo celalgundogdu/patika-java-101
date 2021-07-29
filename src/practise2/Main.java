a7d32fe962435cca1a9f572f8c5ea39a6dce9e9a
Celal Gündoğdu <celalgundogdu42@gmail.com>
2021-07-29 15:31:48 +0300

    practise-2 added

---
+++ src/practise2/Main.java
@@ -0,0 +1,23 @@
+package practise2;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        double cost, taxedCost, taxAmount;
+        float taxRate;
+
+        System.out.print("Tutar: ");
+        cost = input.nextDouble();
+        taxRate = (cost > 0 && cost <= 1000) ? 0.18f : 0.08f;
+        taxAmount = cost * taxRate;
+        taxedCost = cost + taxAmount;
+        System.out.println("KDV oranı: " + taxRate);
+        System.out.printf("KDV miktarı: %.2f\n", taxAmount);
+        System.out.printf("KDV'li tutar: %.2f", taxedCost);
+    }
+
+}
