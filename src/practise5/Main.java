db407bb26b5867e05a7130278bf5ba3df7e941bb
Celal Gündoğdu <celalgundogdu42@gmail.com>
2021-07-29 16:58:27 +0300

    practise-5 added

---
+++ src/practise5/Main.java
@@ -0,0 +1,26 @@
+package practise5;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+            Yarıçapı r, merkez açısısının ölçüsü 𝛼 olan daire diliminin
+            alanı bulan programı yazınız. 𝜋 sayısını  = 3.14 alınız.
+        */
+
+        Scanner input = new Scanner(System.in);
+        double radius, centralAngle, pi=3.14, area;
+
+        System.out.print("Yarıçap: ");
+        radius = input.nextDouble();
+        System.out.print("Merkez açı: ");
+        centralAngle = input.nextDouble();
+
+        area = (pi*(radius*radius)*centralAngle) / 360;
+        System.out.printf("Daire alanı: %.4f", area);
+    }
+
+}
