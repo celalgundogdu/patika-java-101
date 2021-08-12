accebafc0726cdadf8f243817b52fec70dd75bd8
Celal Gundogdu <78298833+celalgundogdu@users.noreply.github.com>
2021-08-12 16:32:17 +0300

    hw-3 added

--- patika_sql_part.sql
+++ patika_sql_part.sql
@@ -34,3 +34,23 @@ WHERE first_name IN('Penelope', 'Nick', 'Ed');
 -- film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız. ( IN operatörünü kullanınız.)
 SELECT * FROM film
 WHERE rental_rate IN(0.99, 2.99, 4.99) AND replacement_cost IN(12.99, 15.99, 28.99);
+
+
+
+-- ÖDEV 3
+
+-- country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
+SELECT country FROM country
+WHERE country LIKE 'A%a';
+
+-- country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
+SELECT country FROM country
+WHERE country LIKE '_____%n';
+
+-- film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
+SELECT title FROM film
+WHERE title ILIKE '%T%T%T%T%';
+
+-- film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
+SELECT * FROM film
+WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
