2fd8e089a3b3dc890433ba48d843e627b7edec68
Celal Gundogdu <78298833+celalgundogdu@users.noreply.github.com>
2021-08-13 17:50:15 +0300

    hw-6 added

--- patika_sql_part.sql
+++ patika_sql_part.sql
@@ -99,3 +99,22 @@ SELECT * FROM customer
 WHERE store_id = 1
 ORDER BY last_name DESC
 LIMIT 4;
+
+
+
+-- ÖDEV 6
+
+-- film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
+SELECT ROUND(AVG(rental_rate), 2) FROM film;
+
+-- film tablosunda bulunan filmlerden kaç tanesi 'C' karekteri ile başlar?
+SELECT COUNT(title) FROM film
+WHERE title LIKE 'C%';
+
+-- film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
+SELECT MAX(length) FROM film
+WHERE rental_rate = 0.99;
+
+-- film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
+SELECT COUNT(DISTINCT replacement_cost) FROM film
+WHERE length > 150;
