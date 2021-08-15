a4f92811970d7185880ba1e5de3fc9b036f25ca2
Celal Gundogdu <78298833+celalgundogdu@users.noreply.github.com>
2021-08-15 20:27:02 +0300

    hw-12 added

--- patika_sql_part.sql
+++ patika_sql_part.sql
@@ -318,3 +318,42 @@ SELECT first_name FROM actor
 EXCEPT ALL
 SELECT first_name FROM customer
 ORDER BY first_name;
+
+
+
+-- ÖDEV 12
+
+-- film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+SELECT COUNT(*) FROM film
+WHERE length > (
+	SELECT AVG(length) From film
+);
+
+-- film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+SELECT COUNT(*) FROM film
+WHERE rental_rate = (
+	SELECT MAX(rental_rate) FROM film
+);
+
+-- film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+SELECT * FROM film
+WHERE rental_rate = ANY (
+	SELECT MIN(rental_rate) FROM film
+) 
+AND
+replacement_cost = ANY (
+	SELECT MIN(replacement_cost) FROM film
+);
+
+-- payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+SELECT customer_id, first_name, last_name FROM customer
+WHERE customer_id = (
+	SELECT customer_id FROM payment
+	GROUP BY customer_id
+	HAVING COUNT(*) = (
+		SELECT COUNT(*) FROM payment
+		GROUP BY customer_id
+		ORDER BY COUNT(*) DESC
+		LIMIT 1
+	)
+);
