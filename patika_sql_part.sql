2ad9f4b969e5cb998d2de66a8af4ae9216faac15
Celal Gundogdu <78298833+celalgundogdu@users.noreply.github.com>
2021-08-15 16:45:56 +0300

    hw-11 added

--- patika_sql_part.sql
+++ patika_sql_part.sql
@@ -280,3 +280,41 @@ RIGHT JOIN payment ON customer.customer_id = payment.customer_id;
 -- customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
 SELECT rental.rental_id, customer.first_name, customer.last_name FROM customer
 FULL JOIN rental ON customer.customer_id = rental.customer_id;
+
+
+
+-- ÖDEV 11
+
+-- actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım.
+SELECT first_name FROM actor
+UNION
+SELECT first_name FROM customer
+ORDER BY first_name;
+
+-- actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
+SELECT first_name FROM actor
+INTERSECT
+SELECT first_name FROM customer
+ORDER BY first_name;
+
+-- actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
+SELECT first_name FROM actor
+EXCEPT
+SELECT first_name FROM customer
+ORDER BY first_name;
+
+-- İlk 3 sorguyu tekrar eden veriler için de yapalım.
+SELECT first_name FROM actor
+UNION ALL
+SELECT first_name FROM customer
+ORDER BY first_name;
+
+SELECT first_name FROM actor
+INTERSECT ALL
+SELECT first_name FROM customer
+ORDER BY first_name;
+
+SELECT first_name FROM actor
+EXCEPT ALL
+SELECT first_name FROM customer
+ORDER BY first_name;
